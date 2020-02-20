71c00fd8fdb5ad7ee5a5238473abe6ece2e27444
mana jalili <jalilimana@gmail.com>
2020-02-20 16:43:24 +0100

    done with the app plus some styling

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,10 +8,6 @@ class DosesController < ApplicationController
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
-    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
-    @dose.ingredient = @ingredient
-    @dose.save
-
 
     if @dose.save
       redirect_to cocktail_path(@cocktail)
@@ -24,14 +20,10 @@ class DosesController < ApplicationController
     @dose = Dose.find(params[:id])
     @dose.destroy
     redirect_to cocktail_path(@dose.cocktail)
-
   end
 
   private
   def dose_params
-    params.require(:dose).permit(:description)
-  end
-  def ingredient_params
-    params.require(:ingredient).permit(:ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
