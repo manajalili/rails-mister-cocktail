f5b8de1c5cd2a1c29cd9b281b5d523f6b4eb50f7
mana jalili <jalilimana@gmail.com>
2020-03-19 18:09:55 +0100

    added the destroy method inside the controller and routes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -35,6 +35,12 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to root_path
+  end
+
 private
   def cocktail_params
     params.require(:cocktail).permit(:name, :photo)
